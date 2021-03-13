9777b303301b5beef9688ccda69a1471a901b869
godofjs <henryeze019@gmail.com>
2021-03-13 20:28:47 +0000

    Mining completed

--- src/controllers/mining/Mining.js
+++ src/controllers/mining/Mining.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const User = require('../../data/models/User');
 module.exports = class Mining {
-    static session = mongoose.startSession();
     static async increaseUsersBalanceByHashRate() {
         const users = await User.find({ isBlocked: false })
         users.map(async (user) => {
             user.balance = user.balance + user.hashRate;
-            console.log(user);
             await user.save();
         })
     }
--- src/services/cron.js
+++ src/services/cron.js
@@ -2,8 +2,7 @@ const cron = require('node-cron');
 const Mining = require('../controllers/mining/Mining');
 cronSchedule(Mining.increaseUsersBalanceByHashRate);
 function cronSchedule(func) {
-    cron.schedule('* * * * *', async function () {
-        console.log('it entered')
+    cron.schedule('0 4 * * *', async function () {
         await func();
     })
 }
