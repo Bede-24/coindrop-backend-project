79fccf7554bc80caa722d229fd1d18e514fe61e1
jakoozi <jamescallum2384@gmail.com>
2021-06-06 01:43:31 +0100

    initial commit

--- src/data/db.js
+++ src/data/db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var mongoDb = 'mongodb+srv://godofjs:$Kingheadthe8th@cluster0.yyvia.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// var mongoDb = 'mongodb+srv://godofjs:$Kingheadthe8th@cluster0.yyvia.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+var mongoDb = 'mongodb+srv://jakoozi:35976421@cluster0.1jrtz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' ;
 mongoose.connect(mongoDb, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
